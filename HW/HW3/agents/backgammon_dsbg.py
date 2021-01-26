688ab640fd68ecfd97cb7c1fab829e4ab49d7658
prism5426 <64817770+prism5426@users.noreply.github.com>
2021-01-25 22:49:40 -0800

    working minimax

--- HW/HW3/agents/backgammon_dsbg.py
+++ HW/HW3/agents/backgammon_dsbg.py
@@ -6,6 +6,8 @@ UW netid(s):
 from game_engine import genmoves
 import math
 
+W = 0
+R = 1
 
 class BackgammonPlayer:
     def __init__(self):
@@ -32,7 +34,7 @@ class BackgammonPlayer:
     # should go down in the search tree. If maxply==-1,
     # no limit is set
     def setMaxPly(self, maxply=-1):
-        self.setMaxPly = maxply
+        self.MaxDepth = maxply
 
     # If not None, it update the internal static evaluation
     # function to be func
@@ -47,77 +49,115 @@ class BackgammonPlayer:
         # Hint: you can get the current player with state.whose_move
         self.die1 = die1
         self.die2 = die2
-
+        self.setMaxPly(3)
         # update the move_generator to current state/position
         self.initialize_move_gen_for_state(state, state.whose_move, self.die1, self.die2)
-        # do minimax for current player
-        best_move_value = self.minimax(state, self.setMaxPly, state.whose_move)
-
-        # do minimax for the layer under current player
         # get all possible moves in current position
         pm_list = self.get_all_possible_moves()
-        # if state.whose_move is W, set initial max value to neg_inf
-        # if state.whose_move is R, set initial max value to inf
-        possible_move_list_value = (-math.inf, math.inf)[state.whose_move]
-        for pair in pm_list:
-            minimax_value = self.minimax(pair[1], self.setMaxPly - 1, not state.whose_move)
-            if state.whose_move is 0: # white
-                possible_move_list_value = max(possible_move_list_value, minimax_value)
-            elif state.whose_move is 1: # red
-                possible_move_list_value = min(possible_move_list_value, minimax_value)
 
-        for pair in pm_list:
-            if pair[1] == possible_move_list_value:
-                return pair[0]
-            
-        return None
+        eval, best_move = self.minimax(state, self.MaxDepth, state.whose_move, None)
+        return best_move
+
 
     # Given a state, returns an integer which represents how good the state is
     # for the two players (W and R) -- more positive numbers are good for W
     # while more negative numbers are good for R
     def staticEval(self, state):
         pts = 0
-        for index in range(24):
+        for index in range(6, 18):
+            white_count = 0
+            red_count = 0
             for checker in state.pointLists[index]:
-                if checker == 'W':
+                if checker == W:
+                    white_count += 1
                     pts += index
-                elif checker == 'R':
+                elif checker == R:
+                    red_count += 1
                     pts -= (23 - index)
+            if white_count >= 2:
+                pts += 15
+            if red_count >= 2:
+                pts -= 15
 
+        for index in range(0, 6):
+            white_count = 0
+            red_count = 0
+            for checker in state.pointLists[index]:
+                if checker == W:
+                    white_count += 1
+                    if index == 0 or index == 5:
+                        pts += 25
+                    pts += 5
+                elif checker == R:
+                    red_count += 1
+                    if index == 0 or index == 5:
+                        pts -= 25
+                    pts -= 25
+            if white_count >= 2:
+                pts += 15
+            if red_count >= 2:
+                pts -= 15
+
+        for index in range(18, 24):
+            white_count = 0
+            red_count = 0
+            for checker in state.pointLists[index]:
+                if checker == W:
+                    white_count += 1
+                    if index == 18 or index == 23:
+                        pts += 25
+                    pts += 25
+                elif checker == R:
+                    red_count += 1
+                    if index == 18 or index == 23:
+                        pts -= 25
+                    pts -= 5
+            if white_count >= 2:
+                pts += 15
+            if red_count >= 2:
+                pts -= 15
+
+        for checker in state.bar:
+            if checker == W:
+                pts -= 5
+            elif checker == R:
+                pts += 5
+
+        pts += len(state.white_off) * 100
+        pts -= len(state.red_off) * 100
         return pts
 
     # returns the best move's staticEval
-    def minimax(self, state, depth, maximizing_player):
+    def minimax(self, state, depth, maximizing_player, best_move):
+        if state is None:
+            return 0, 'p'
         if depth == 0 or self.gameover(state, maximizing_player):
-            return self.staticEval(state)
+            return self.staticEval(state), best_move
 
-        if maximizing_player:
+        # update the move_generator to current state/position
+        self.initialize_move_gen_for_state(state, maximizing_player, self.die1, self.die2)
+        # get all possible moves in current position
+        pm_list = self.get_all_possible_moves()
+
+        if maximizing_player is W:
             maxEval = -math.inf
-
-            # update the move_generator to current state/position
-            self.initialize_move_gen_for_state(state, maximizing_player, self.die1, self.die2)
-            # get all possible moves in current position
-            pm_list = self.get_all_possible_moves()
-
             for s in pm_list:
-                eval = self.minimax(s, depth - 1, not maximizing_player)
-                maxEval = max(maxEval, eval)
-            return maxEval
+                eval, _ = self.minimax(s[1], depth-1, not maximizing_player, best_move)
+                # maxEval = max(maxEval, eval)
+                if eval > maxEval:
+                    maxEval = eval
+                    best_move = s[0]
+            return maxEval, best_move
 
         else:
             minEval = math.inf
-
-            # update the move_generator to current state/position
-            self.initialize_move_gen_for_state(state, maximizing_player, self.die1, self.die2)
-            # get all possible moves in current position
-            pm_list = self.get_all_possible_moves()
-
-            for pair in pm_list:
-                # pair[0] is move, pair[1] is state
-                eval = self.minimax(pair[1], depth - 1, not maximizing_player)
-                minEval = min(minEval, eval)
-            return minEval
-
+            for s in pm_list:
+                eval, _ = self.minimax(s[1], depth-1, not maximizing_player, best_move)
+                # inEval = min(minEval, eval)
+                if eval < minEval:
+                    minEval = eval
+                    best_move = s[0]
+            return minEval, best_move
 
     def initialize_move_gen_for_state(self, state, who, die1, die2):
         self.move_generator = self.GenMoveInstance.gen_moves(state, who, die1, die2)
@@ -140,9 +180,9 @@ class BackgammonPlayer:
                 # print("next returns: ",m[0]) # Prints out the move.    For debugging.
                 if m[0] != 'p':
                     any_non_pass_moves = True
-                    move_list.append((m[0], m[1]))    # Add the move to the list.
+                    move_list.append(m)    # Add the move to the list.
             except StopIteration as e:
                 done_finding_moves = True
         if not any_non_pass_moves:
-            move_list.append('p')
+            move_list.append(('p', None))
         return move_list
